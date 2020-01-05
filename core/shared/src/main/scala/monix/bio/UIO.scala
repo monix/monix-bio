c256cfb1c4bb100b85b8129d50d482312e4689f2
aleksandar.skrbic <aleksandar.skrbic@smartcat.io>
2020-01-05 22:46:04 +0100

    Initial commit

--- core/shared/src/main/scala/monix/bio/UIO.scala
+++ core/shared/src/main/scala/monix/bio/UIO.scala
@@ -17,73 +17,122 @@
 
 package monix.bio
 
+import monix.bio.internal.{TaskRace, TaskRacePair, TaskShift}
+import monix.execution.Scheduler
+
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
 
 object UIO {
 
   /**
-    * @see See [[monix.bio.BIO.apply]]
-    */
+   * @see See [[monix.bio.BIO.apply]]
+   */
   def apply[A](a: => A): UIO[A] =
     BIO.Eval(a _)
 
   /**
-    * @see See [[monix.bio.BIO.now]]
-    */
+   * @see See [[monix.bio.BIO.now]]
+   */
   def now[A](a: A): UIO[A] =
     BIO.now(a)
 
   /**
-    * @see See [[monix.bio.BIO.pure]]
-    */
+   * @see See [[monix.bio.BIO.pure]]
+   */
   def pure[A](a: A): UIO[A] =
     BIO.pure(a)
 
   /**
-    * @see See [[monix.bio.BIO.raiseFatalError]]
-    */
+   * @see See [[monix.bio.BIO.raiseFatalError]]
+   */
   def raiseFatalError(ex: Throwable): UIO[Nothing] =
     BIO.raiseFatalError(ex)
 
   /**
-    * @see See [[monix.bio.BIO.defer]]
-    */
+   * @see See [[monix.bio.BIO.defer]]
+   */
   def defer[A](fa: => UIO[A]): UIO[A] =
     BIO.defer(fa)
 
   /**
-    * @see See [[monix.bio.BIO.suspend]]
-    */
+   * @see See [[monix.bio.BIO.deferAction]]
+   */
+  def deferAction[E, A](f: Scheduler => UIO[A]): UIO[A] =
+    BIO.deferAction(f)
+
+  /**
+   * @see See [[monix.bio.BIO.suspend]]
+   */
   def suspend[A](fa: => UIO[A]): UIO[A] =
     BIO.suspend(fa)
 
   /**
-    * @see See [[monix.bio.BIO.eval]]
-    */
+   * @see See [[monix.bio.BIO.eval]]
+   */
   def eval[A](a: => A): UIO[A] =
     BIO.Eval(a _)
 
   /**
-    * @see See [[monix.bio.BIO.evalAsync]]
-    */
+   * @see See [[monix.bio.BIO.evalAsync]]
+   */
   def evalAsync[A](a: => A): UIO[A] =
     BIO.Eval(a _).executeAsync
 
   /**
-    * @see See [[monix.bio.BIO.never]]
-    */
+   * @see See [[monix.bio.BIO.delay]]
+   */
+  def delay[A](a: => A): UIO[A] =
+    eval(a)
+
+  /**
+   * @see See [[monix.bio.BIO.never]]
+   */
   val never: UIO[Nothing] =
     BIO.never
 
   /**
-    * @see See [[monix.bio.BIO.unit]]
-    */
+   * @see See [[monix.bio.BIO.tailRecM]]
+   */
+  def tailRecM[A, B](a: A)(f: A => UIO[Either[A, B]]): UIO[B] =
+    defer(f(a)).flatMap {
+      case Left(continueA) => tailRecM(continueA)(f)
+      case Right(b)        => now(b)
+    }
+
+  /**
+   * @see See [[monix.bio.BIO.unit]]
+   */
   val unit: UIO[Unit] =
     BIO.unit
 
   /**
-    * @see See [[monix.bio.BIO.sleep]]
-    */
+   * @see See [[monix.bio.BIO.cancelBoundary]]
+   */
+  val cancelBoundary: UIO[Unit] =
+    BIO.cancelBoundary
+
+  /**
+   * @see See [[monix.bio.BIO.race]]
+   */
+  def race[A, B](fa: UIO[A], fb: UIO[B]): UIO[Either[A, B]] =
+    TaskRace(fa, fb)
+
+  /**
+   * @see See [[monix.bio.BIO.shift]]
+   */
+  val shift: UIO[Unit] =
+    BIO.shift
+
+  /**
+   * @see See [[monix.bio.BIO.shift]]
+   */
+  def shift(ec: ExecutionContext): UIO[Unit] =
+    BIO.shift(ec)
+
+  /**
+   * @see See [[monix.bio.BIO.sleep]]
+   */
   def sleep(timespan: FiniteDuration): UIO[Unit] =
     BIO.sleep(timespan)
 }
