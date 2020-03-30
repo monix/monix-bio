4ef0d03c1c837570dead2ac455794bcdf6722649
Richard <someoneoutthere2009@live.com>
2020-03-30 20:11:56 +0200

    Ran scalafmtAll and coreJVM/test

--- core/shared/src/main/scala/monix/bio/internal/UIOEvalAsync.scala
+++ core/shared/src/main/scala/monix/bio/internal/UIOEvalAsync.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019-2020 by The Monix Project Developers.
+ * See the project homepage at: https://monix.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package monix.bio.internal
 
 import monix.bio.{BIO, BiCallback, UIO}
@@ -20,7 +37,6 @@ private[bio] object UIOEvalAsync {
   // Implementing Async's "start" via `ForkedStart` in order to signal
   // that this is a task that forks on evaluation
   private final class EvalAsyncRegister[A](a: () => A) extends ForkedRegister[Nothing, A] {
-
     def apply(ctx: BIO.Context[Nothing], cb: BiCallback[Nothing, A]): Unit =
       ctx.scheduler.executeAsync(() => {
         ctx.frameRef.reset()
