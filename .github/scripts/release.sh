1d7713f1ef23e23407c8ce8862ab20041a24dbba
Avasil <pgawrys2@gmail.com>
2020-06-05 17:27:59 +0200

    try not setting envs in release script

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -3,5 +3,5 @@
 set -e
 
 sbt ci-release
-SCALAJS_VERSION=0.6.33 sbt ci-release
-SCALAJS_VERSION=1.0.1 sbt ci-release
+# SCALAJS_VERSION=0.6.33 sbt ci-release
+# SCALAJS_VERSION=1.0.1 sbt ci-release
