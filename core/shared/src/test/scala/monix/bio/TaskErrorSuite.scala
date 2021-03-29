6a755867246100c0ae09670eaa2a7dcf41a43ce3
Rohan Sircar <rohansircar@protonmail.com>
2021-03-29 15:46:35 +0530

    Add test cases for mapErrorPartial/With

--- core/shared/src/test/scala/monix/bio/TaskErrorSuite.scala
+++ core/shared/src/test/scala/monix/bio/TaskErrorSuite.scala
@@ -23,7 +23,6 @@ import monix.execution.internal.Platform
 import scala.concurrent.TimeoutException
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
-import java.nio.file.NoSuchFileException
 
 object TaskErrorSuite extends BaseTestSuite {
   test("IO.attempt should expose typed error") { implicit s =>
@@ -339,7 +338,7 @@ object TaskErrorSuite extends BaseTestSuite {
   test("IO#mapErrorPartial should handle recoverable exceptions") { implicit s =>
     val ex = DummyException("dummy")
     val io: IO[String, Int] = IO(throw ex).mapErrorPartial { case e: DummyException =>
-      IO.raiseError(e.message)
+      e.message
     }
 
     val f = io.attempt.runToFuture
@@ -349,7 +348,29 @@ object TaskErrorSuite extends BaseTestSuite {
 
   test("IO#mapErrorPartial should terminate on non-recoverable exceptions") { implicit s =>
     val ex = DummyException("dummy")
-    val io: IO[String, Int] = IO(throw ex).mapErrorPartial { case e: NoSuchFileException =>
+    val io: IO[String, Int] = IO(throw ex).mapErrorPartial { case e: IllegalArgumentException =>
+      "not found"
+    }
+
+    val f = io.attempt.runToFuture
+    s.tick()
+    assertEquals(f.value, Some(Failure(ex)))
+  }
+
+  test("IO#mapErrorPartialWith should handle recoverable exceptions") { implicit s =>
+    val ex = DummyException("dummy")
+    val io: IO[String, Int] = IO(throw ex).mapErrorPartialWith { case e: DummyException =>
+      IO.raiseError(e.message)
+    }
+
+    val f = io.attempt.runToFuture
+    s.tick()
+    assertEquals(f.value, Some(Success(Left("dummy"))))
+  }
+
+  test("IO#mapErrorPartialWith should terminate on non-recoverable exceptions") { implicit s =>
+    val ex = DummyException("dummy")
+    val io: IO[String, Int] = IO(throw ex).mapErrorPartialWith { case e: IllegalArgumentException =>
       IO.raiseError("not found")
     }
 
